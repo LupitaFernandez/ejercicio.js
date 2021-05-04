ac8bd50f9a77d8fd4feb0c58f0b7107092fd2dbd
LupitaFernandez <fernandez.guada@hotmail.com>
2021-05-04 15:52:23 -0300

    new change

--- app.js
+++ app.js
@@ -1,38 +1,67 @@
 // Requerimos el módulo 'process'
 let process = require('process');
 let fs= require ('fs');
-let moduloTareas = require ('./modulos/moduloTareas')
+let moduloTareas = require ('./modulos/moduloTareas');
 
 // Capturamos el comando que el usuario ingresó por consola
 let comandoDelUsuario = process.argv[2];
 
+
+
+
 switch(comandoDelUsuario) {
+    //Case: listar                              
 	case 'listar':
-        let listaParseada =moduloTareas.traerArrayDeTareas ();
-    listaParseada.forEach (function (elemento, posicion){
-        console.log(`${elemento.titulo} - ${elemento.estado}`);
+        let listaParseada =moduloTareas.traerArrayDeTareas (); //Obtener listado
+    listaParseada.forEach (function (elemento, posicion){ //Parsear listado obtenido
+        console.log(`${elemento.titulo} - ${elemento.estado}`); //Imprimir una por una las tareas
    
 })
     break;
 
-    case 'crearTarea':
-        let tituloObtenido = process.argv [3]
-        let nuevaTarea = {
-            titulo: tituloObtenido,
-            estado: "terminada"
+
+    case 'agregar':
+        let nuevoTitulo = process.argv[3];
+        if (nuevoTitulo == undefined){
+            console.log("Tenes que escribir un titulo");
+            break;
         }
-        moduloTareas.crearNuevaTarea (nuevaTarea);
-        console.log ('Tarea creada con éxito')
+        let nuevaTarea = {
+            titulo: nuevoTitulo,
+            estado: "pendiente"
+        }
 
+        moduloTareas.escribirJSON(nuevaTarea);
+            console.log("Tu tarea ha sido añadida");
+            
+            break;
+
+
+        //Case 'filtrar'
+        case "filtrar": 
+        var estado = process.argv[3]; 
+        if (estado == undefined){ //Si el usuario no escribe ningún estado muestro en consola un mensaje de error
+            console.log("Tenes que filtrar por algo");
+            break;
+        }
+        let listaFiltrada = moduloTareas.filtrarJSON(estado); 
+        listaFiltrada.forEach(function(tarea){ //Recibe por parametro (tarea) 
+            console.log(tarea.titulo + " - " + tarea.estado) //Muestra por consola cada titulo y el estado
+        })
         break;
 
-    case 'leerPorEstado':
-        let estadoEnProceso = process.argv [4]
-        moduloTareas.leerPorEstado ('terminada')
-      console.log (`${elemento.titulo} - ${elemento.estado}`)
+
+
+        //Case 'cambiar'
+    case "cambiar": 
+        var estado = process.argv[3]; 
+        var nuevoEstado = process.argv[4]; //Tomo el nuevo estado de la quinta posición del array 
+        moduloTareas.cambiarEstado(estado,nuevoEstado) //Ejecuto el método 'cambiarEstado' del 'moduloTareas'. Le paso dos parámetros: el estado que quiero cambiar y el nuevo estado.
+
+            console.log("Se realizaron todos los cambios");
         
         break;
-        
+
 
         // También podemos agregar acciones para cuando el usuario no ingresa un comando...
     case undefined:
--- modulos/moduloTareas.js
+++ modulos/moduloTareas.js
@@ -1,26 +1,42 @@
-let fs = require ('fs')
+let fs = require('fs')
 
 let moduloTareas = {
-    traerArrayDeTareas: function (){
-        let listaDeTareas = fs.readFileSync ('tareas.json', 'utf-8');
-        return JSON.parse (listaDeTareas);
+    archivo: './tareas.json',
+    leerJSON: function() {
+        let listaDeTareas = fs.readFileSync(this.archivo, 'utf-8');
+        let tareas = JSON.parse(listaDeTareas);
+        return tareas
     },
-    crearNuevaTarea: function (tarea){
-        let listaParseada= this.traerArrayDeTareas ();
-        listaParseada.push (tarea)
-        let listaComoJSON = JSON.stringify (listaParseada);
-        fs.writeFileSync ('tareas.json', listaComoJSON);
+    escribirJSON: function(nuevaTarea) {
+        let tareasAnteriores = this.leerJSON();
+        tareasAnteriores.push(nuevaTarea);
+        let tareasActualizadas = JSON.stringify(tareasAnteriores);
+        fs.writeFileSync(this.archivo, tareasActualizadas, 'utf-8')
     },
-    leerPorEstado: function (estado) {
-        let estado = "terminada"
-       let tareas = this.traerArrayDeTareas ();
-       let estadoFiltrado = tareas.filter (function (estado){
-           return estadoFiltrado;
-       })
+    filtrarJSON:function(estado){ 
+        let listaDeTareas = this.leerJSON(); //Traer el json parseado
+        let listaFiltrada = listaDeTareas.filter(function(tarea){ //Método '.filter()': recibe un callback cuyo parametro es el elemento en cada iteración
+            return tarea.estado == estado
+        })
+        return listaFiltrada
+    },
+    cambiarEstado:function(estado,nuevoEstado){
+        let listaDeTareas = this.leerJSON(); //Ejecuto el metodo 'leerJSON' del 'moduloTareas'
+
+        let Tarea = function(titulo,estado){ //Creo un constructor para para ser usado luego para crear tareas
+            this.titulo = titulo, //Propiedad 'titulo' => parametro titulo
+            this.estado = estado //Propiedad 'estado' => parametro estado
+        }
+
+        let listaModificada = listaDeTareas.map(function(tarea){ //Ejecuto método '.map()' a la lista de tareas, cuyo callback recibe como parámetro el elemento del array a iterar
+            if(tarea.estado == estado){ //Estado del elemento coincide con el pasado por parametro de 'cambiarEstado'
+                tarea.estado = nuevoEstado //cambio el estado por el que viene como parámetro 'nuevoEstado'
+            }
+            return new Tarea(tarea.titulo,tarea.estado) //Retorna un nuevo elemento usando para ello el constructor anteriormente creado para tal efecto, de manera que 'listaModificada' se va llenando no solo de los elementos modificados, sino tambien de los que no se modificaron.
+        })
+        let tareasActualizadas = JSON.stringify(listaModificada); //Convierto el array de objeto en formato json
+        fs.writeFileSync(this.archivo, tareasActualizadas, 'utf-8') //Guardo el json en el archivo correspondiente pasando los argumentos que requier el método '.writeFileSync'
     }
 }
 
-
-
-
-module.exports=moduloTareas;
+module.exports = moduloTareas;
